5a2703c6f4263f8182db9882a776200b73842528
amosvoron <amos.voron@gmail.com>
2020-12-07 23:15:54 +0100

    refactor: Clean production code

--- etl.py
+++ etl.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import glob
 import psycopg2
 import pandas as pd
@@ -6,6 +7,17 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
+    """Extracts songs and artists from the JSON data source
+    and inserts them into the database.
+    
+    Args:
+        cur: The PostgreSQL cursor object. 
+        filepath (str): The filepath to the JSON data source.
+        
+    Returns:
+        None
+    """
+    
     # open song file
     df = pd.read_json(filepath, lines=True)
 
@@ -19,6 +31,16 @@ def process_song_file(cur, filepath):
 
     
 def process_log_file(cur, filepath):
+    """Extracts time data, users and songplay data from the JSON data source
+    and inserts them into the database.
+    
+    Args:
+        cur: The PostgreSQL cursor object. 
+        filepath (str): The filepath to the JSON data source.
+        
+    Returns:
+        None
+    """    
     # open log file
     df = pd.read_json(filepath, lines=True)
 
@@ -44,9 +66,13 @@ def process_log_file(cur, filepath):
         cur.execute(time_table_insert, tuple(row))  
 
     # load user table
-    # Note: We extract all distict users
-    # Remark on the 'level' column:
-    #   
+    #
+    # ATTENTION:
+    # We extract user data by using 5 keys (userId, firstName, lastName, gender, level).
+    # The first four keys guarantee the uniqueness of the userId which is a primary key.
+    # Since the level key is not user-related but user-time-related attribute there will still
+    # remain some duplicates in cases when user changes the subscription level. These duplicates
+    # will be handled by ON CONFLICT DO UPDATE clause providing the last level in sequence.
     user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']].drop_duplicates()
         
     # insert user records
@@ -81,6 +107,17 @@ def process_log_file(cur, filepath):
 
     
 def process_data(cur, conn, filepath, func):
+    """Processes all data through the ETL pipeline.
+    
+    Args:
+        cur: The PostgreSQL cursor object. 
+        conn: The PostgreSQL connection object.
+        filepath (str): The filepath to the JSON data source.
+        func: The ETL data source specific function to call.
+        
+    Returns:
+        None
+    """        
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
@@ -100,6 +137,18 @@ def process_data(cur, conn, filepath, func):
 
         
 def database_check(cur):
+    """Performs database check operations like rowcount checks
+    and database referential integrity check.
+    
+    If the database passes the referential integrity check,
+    the foreign keys are created.
+    
+    Args:
+        cur: The PostgreSQL cursor object. 
+    
+    Returns:
+        None
+    """        
     
     print('\n-----------------------------------------')
     print('  Table rowcount check')
@@ -116,22 +165,42 @@ def database_check(cur):
     print('  Database referential integrity check ')
     print('-----------------------------------------')
     cur.execute(database_integrity_check)
-    print(f'Matched rows: {cur.fetchone()[0]} of {total_rows}')
+    matched_rows = cur.fetchone()[0]
+    print(f'Matched rows: {matched_rows} of {total_rows}')
+    
+    # Create foreign keys if the database passes the referential integrity check
+    if matched_rows == total_rows:
+        cur.execute(create_foreign_keys)
+        print('Foreign keys created.')
+    else:
+        print(f'Database did not pass the referential integrity check.')
     print()
     
 
 def main():
+    
+    # Check input paramater - the root directory
+    if len(sys.argv) < 2:
+        print('Missing parameter: Please provide a data root directory parameter.' +
+              '\nPay attention that the song and log data subdirectories must be located inside the root directory:' +
+              '\n  /song_data'
+              '\n  /log_data')
+        return
+    
+    data_dir = sys.argv[1]
+    if os.path.isdir(data_dir) == False:
+        print(f'Invalid parameter: Data directory "{data_dir}" does not exist.\n')
+        return
+        
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
-    
-    data_dir = 'D:/udacity/Data Engineering/project 1/data/'
-    
+     
     print('Song data processing started...')
-    process_data(cur, conn, filepath=data_dir+'song_data', func=process_song_file)
+    process_data(cur, conn, filepath=os.path.join(data_dir, 'song_data'), func=process_song_file)
     print('Song data processing completed.')
     
     print('Log data processing started...')
-    process_data(cur, conn, filepath=data_dir+'log_data', func=process_log_file)
+    process_data(cur, conn, filepath=os.path.join(data_dir, 'log_data'), func=process_log_file)
     print('Log data processing completed.')
     
     # check database
